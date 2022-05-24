c1be925f7bb75c641e12c3eecf198c039753e2c1
José Luis Bueno <jlbp@uma.es>
2022-05-24 11:15:50 +0200

    first test

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -1,2 +1,17 @@
-package org.mps.authentication;public class IntegrationTestIT {
+package org.mps.authentication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTestIT {
+
+    @Test
+    public void testCredentialStoreWithValidArgumentsAndNotRepeatedReturnsVALIDATION_OK(){
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password,123");
+        var credentialStore = new CredentialStoreSet();
+        var credentialStoreValidator = new CredentialValidator(birthDate, passwordString, credentialStore);
+        assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.VALIDATION_OK);
+    }
 }
