680db7dd563a9854a4e72496bfb8123ef7ac03ca
NicoRomero-07 <nicoromero07@uma.es>
2022-05-24 11:29:36 +0200

    testUserRegistrationRegisterWithValidArgumentsAndRepeatedNotIncreasesCredentialStoreSize

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -15,7 +15,25 @@ public class IntegrationTestIT {
         userRegistration.register(birthDate, passwordString, credentialStore);
         assertEquals(credentialStore.size(), 1);
     }
-
+    @Test
+    public void testUserRegistrationRegisterWithInvalidPasswordAndNotRepeatedNotIncreasesCredentialStoreSize(){
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password123");
+        var credentialStore = new CredentialStoreSet();
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.register(birthDate,passwordString,credentialStore);
+        assertEquals(0, credentialStore.size());
+    }
+    @Test
+    public void testUserRegistrationRegisterWithValidArgumentsAndRepeatedNotIncreasesCredentialStoreSize(){
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password,123");
+        var credentialStore = new CredentialStoreSet();
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.register(birthDate,passwordString,credentialStore);
+        userRegistration.register(birthDate,passwordString,credentialStore);
+        assertEquals(1, credentialStore.size());
+    }
     @Test
     public void testCredentialStoreWithValidArgumentsAndNotRepeatedReturnsVALIDATION_OK(){
         Date birthDate = new Date(1,1,2001);
@@ -24,15 +42,6 @@ public class IntegrationTestIT {
         var credentialStoreValidator = new CredentialValidator(birthDate, passwordString, credentialStore);
         assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.VALIDATION_OK);
 
-    }
-    @Test
-    public void testUserRegistrationWithInvalidPasswordAndNotRepeatedReturnsPASSWORD_INVALID(){
-        Date birthDate = new Date(1,1,2001);
-        var passwordString = new PasswordString("password123");
-        var credentialStore = new CredentialStoreSet();
-        UserRegistration userRegistration = new UserRegistration();
-        userRegistration.register(birthDate,passwordString,credentialStore);
-        assertEquals(0, credentialStore.size());
     }
 
     @Test
