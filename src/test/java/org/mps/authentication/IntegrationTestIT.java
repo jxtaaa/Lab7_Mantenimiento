f5f17317ccdd46d14778bd861d22cbe172f103fe
José Luis Bueno <jlbp@uma.es>
2022-05-24 11:25:06 +0200

    more tests

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -7,6 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IntegrationTestIT {
 
     @Test
+    public void testUserRegistrationRegisterWithValidArgumentsIncreasesCredentialStoreSize(){
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password,123");
+        var credentialStore = new CredentialStoreSet();
+        var userRegistration = new UserRegistration();
+        userRegistration.register(birthDate, passwordString, credentialStore);
+        assertEquals(credentialStore.size(), 1);
+    }
+
+    @Test
     public void testCredentialStoreWithValidArgumentsAndNotRepeatedReturnsVALIDATION_OK(){
         Date birthDate = new Date(1,1,2001);
         var passwordString = new PasswordString("password,123");
@@ -14,4 +24,15 @@ public class IntegrationTestIT {
         var credentialStoreValidator = new CredentialValidator(birthDate, passwordString, credentialStore);
         assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.VALIDATION_OK);
     }
+
+    @Test
+    public void testCredentialStoreWithInvalidBirthDateAndNotRepeatedReturnsBIRTHDAY_INVALID() {
+        Date birthDate = new Date(31, 2, 2001);
+        var passwordString = new PasswordString("password,123");
+        var credentialStore = new CredentialStoreSet();
+        var credentialStoreValidator = new CredentialValidator(birthDate, passwordString, credentialStore);
+        assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.BIRTHDAY_INVALID);
+    }
+
+
 }
