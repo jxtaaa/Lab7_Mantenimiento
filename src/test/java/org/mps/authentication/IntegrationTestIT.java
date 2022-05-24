68eea4d98c6143971ad7b496a0cc2c83aa1a40f2
NicoRomero-07 <nicoromero07@uma.es>
2022-05-24 12:59:45 +0200

    test Integration with mocks

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -26,4 +26,52 @@ public class IntegrationTestIT {
 
         assertEquals(1, credentialStore.size());
     }
+    @Test
+    public void testLevel3_1(){
+        UserRegistration userRegistration = new UserRegistration();
+
+        Date date = new Date(10,3,2001);
+        PasswordString mockPassword = mock(PasswordString.class);
+        CredentialStore credentialStore = mock(CredentialStore.class);
+        CredentialValidator credentialValidator = new CredentialValidator(date,mockPassword,credentialStore);
+
+        when(mockPassword.validate()).thenReturn(true);
+        when(credentialStore.credentialExists(date,mockPassword)).thenReturn(false);
+
+        userRegistration.register(date, mockPassword,credentialStore,credentialValidator);
+
+        when(credentialStore.size()).thenReturn(1);
+
+        assertEquals(1, credentialStore.size());
+    }
+
+    @Test
+    public void testLevel4_1(){
+        UserRegistration userRegistration = new UserRegistration();
+
+        Date date = new Date(10,3,2001);
+        PasswordString password = new PasswordString("password,1234");
+        CredentialStore credentialStore = mock(CredentialStore.class);
+        CredentialValidator credentialValidator = new CredentialValidator(date,password,credentialStore);
+        when(credentialStore.credentialExists(date,password)).thenReturn(false);
+        userRegistration.register(date, password,credentialStore,credentialValidator);
+
+        when(credentialStore.size()).thenReturn(1);
+
+        assertEquals(1, credentialStore.size());
+    }
+
+    @Test
+    public void testLevel5(){
+        UserRegistration userRegistration = new UserRegistration();
+
+        Date date = new Date(10,3,2001);
+        PasswordString password = new PasswordString("password,1234");
+        CredentialStore credentialStore = new CredentialStoreSet();
+        CredentialValidator credentialValidator = new CredentialValidator(date,password,credentialStore);
+
+        userRegistration.register(date, password,credentialStore,credentialValidator);
+
+        assertEquals(1, credentialStore.size());
+    }
 }
