2ce7bc53a875abffc62c11a61f19e2766cf66477
NicoRomero-07 <nicoromero07@uma.es>
2022-05-24 11:25:59 +0200

    testUserRegistrationWithInvalidPasswordAndNotRepeatedReturnsPASSWORD_INVALID

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -15,4 +15,13 @@ public class IntegrationTestIT {
         assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.VALIDATION_OK);
 
     }
+    @Test
+    public void testUserRegistrationWithInvalidPasswordAndNotRepeatedReturnsPASSWORD_INVALID(){
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password123");
+        var credentialStore = new CredentialStoreSet();
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.register(birthDate,passwordString,credentialStore);
+        assertEquals(0, credentialStore.size());
+    }
 }
