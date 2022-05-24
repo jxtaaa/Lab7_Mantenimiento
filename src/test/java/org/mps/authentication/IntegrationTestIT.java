4b3e935374df6622b333197ab9ccf7d0e5dbeae2
José Luis Bueno <jlbp@uma.es>
2022-05-24 11:44:32 +0200

    more tests

--- src/test/java/org/mps/authentication/IntegrationTestIT.java
+++ src/test/java/org/mps/authentication/IntegrationTestIT.java
@@ -15,6 +15,16 @@ public class IntegrationTestIT {
         userRegistration.register(birthDate, passwordString, credentialStore);
         assertEquals(credentialStore.size(), 1);
     }
+
+    @Test
+    public void testUserRegistrationRegisterWithInvalidBirthDateAndNotRepeatedNotIncreasesCredentialStoreSize(){
+        Date birthDate = new Date(31,2,2001);
+        var passwordString = new PasswordString("password,123");
+        var credentialStore = new CredentialStoreSet();
+        var userRegistration = new UserRegistration();
+        userRegistration.register(birthDate, passwordString, credentialStore);
+        assertEquals(credentialStore.size(), 0);
+    }
     @Test
     public void testUserRegistrationRegisterWithInvalidPasswordAndNotRepeatedNotIncreasesCredentialStoreSize(){
         Date birthDate = new Date(1,1,2001);
@@ -71,6 +81,17 @@ public class IntegrationTestIT {
         credentialStore.register(birthDate, passwordString);
         assertEquals(credentialStoreValidator.validate(), CredentialValidator.ValidationStatus.EXISTING_CREDENTIAL);
     }
+
+    @Test
+    public void testCredentialStoreRegisterWithValidArgumentsIncreasesCredentialStoreSize(){
+        var credentialStore = new CredentialStoreSet();
+        Date birthDate = new Date(1,1,2001);
+        var passwordString = new PasswordString("password,123");
+        credentialStore.register(birthDate, passwordString);
+        assertEquals(credentialStore.size(), 1);
+    }
+
+
 
 
 }
